90603c8110de767efd69901b6455e4da32070eeb
IAmParasite <1004183196@qq.com>
2020-12-06 20:35:30 +0800

    修改了获取个人信息的方式
    post -> get，通过api传参

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -75,33 +75,57 @@ export default {
         },
         getInfo() {
             let formData = new FormData();
-            console.log(this.$route.params.userid);
-            console.log(localStorage.getItem('userid'));
-            formData.append('user_id', 1);
+            //console.log(this.$route.params.userid);
+            //console.log(localStorage.getItem('userid'));
+            //formData.append('user_id', 1);
             let config = {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
             };
             var _this = this;
-            axios.post('https://go-service-296709.df.r.appspot.com/api/v1/user/return/account_info', formData, config)
-            .then(function(response) {
+            axios.get('https://go-service-296709.df.r.appspot.com/api/v1/user/return/account_info/' + localStorage.getItem('userid')).then(response => {
                 if(response) {
-                    if(response.data.success) {
-                        console.log(response.data)
-                        _this.userinfo.username = response.data.data.username
-                        _this.userinfo.email = response.data.data.email
-                        _this.userinfo.basic_info = response.data.data.basic_info
-                        _this.userinfo.password = response.data.data.password
+                     if(response) {
+                        if(response.data.success) {
+                            console.log(response)
+                            _this.userinfo.username = response.data.data.username
+                            _this.userinfo.email = response.data.data.email
+                            _this.userinfo.basic_info = response.data.data.basic_info
+                            _this.userinfo.password = response.data.data.password
+                        }
+                        else {
+                            console.log(userid)
+                            console.log(response.data)
+                            console.log("获取失败 " + response.data)
+                        }
                     }
-                    else {
-                        console.log("获取失败 " + response.data.message)
-                    }
-                    //_this.username = response.data.
                 }
-            }).catch(error=> {
-                console.log('error', error);
+                else {
+                    console.log("error");
+                }
             })
+            // var _this = this;
+            // axios.post('https://go-service-296709.df.r.appspot.com/api/v1/user/return/account_info/:userid', formData, config)
+            // .then(function(response) {
+                // if(response) {
+                //     if(response.data.success) {
+                //         console.log(response)
+                //         _this.userinfo.username = response.data.data.username
+                //         _this.userinfo.email = response.data.data.email
+                //         _this.userinfo.basic_info = response.data.data.basic_info
+                //         _this.userinfo.password = response.data.data.password
+                //     }
+                //     else {
+                //         console.log(userid)
+                //         console.log(response.data)
+                //         console.log("获取失败 " + response.data)
+                //     }
+                //     //_this.username = response.data.
+                // }
+            // }).catch(error=> {
+            //     console.log('error', error);
+            // })
         }
     },
     mounted() {
