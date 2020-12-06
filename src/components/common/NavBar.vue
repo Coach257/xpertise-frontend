53e5d6884155474148196e091f6efe51b0fcb710
alphar <alpharpledez@outlook.com>
2020-12-06 20:35:18 +0800

    加上了登录注册个人中心按钮

--- src/components/common/NavBar.vue
+++ src/components/common/NavBar.vue
@@ -2,22 +2,45 @@
   <div id="nav_bar">
     <div class="nav_bar_frame">
       <div class="nav_bar_icon">
-          <div class="nav_bar_icon_title_bold">Xpertise</div>
-          <div class="nav_bar_icon_title">  Scholar</div>
+        <div class="nav_bar_icon_title_bold">Xpertise</div>
+        <div class="nav_bar_icon_title">Scholar</div>
       </div>
       <div class="nav_bar_item_outer_frame">
-          <router-link :to="item.url" tag="div" class="nav_bar_item_frame" v-for="(item,i) in pages" :key=item @click="onIndexChange(i)">
-              
-              <div class="nav_bar_active_item" v-if="i===activeIndex">
-                 {{item.title}}
-              </div>
-              <div class="nav_bar_inactive_item" v-else>
-                  {{item.title}}
-              </div>
-              </router-link>
-          
-     </div>
-      <div class="nav_bar_action_frame"></div>
+        <router-link
+          :to="item.url"
+          tag="div"
+          class="nav_bar_item_frame"
+          v-for="(item, i) in pages"
+          :key="item"
+          @click="onIndexChange(i)"
+        >
+          <div class="nav_bar_active_item" v-if="i === active_index">
+            {{ item.title }}
+          </div>
+          <div class="nav_bar_inactive_item" v-else>
+            {{ item.title }}
+          </div>
+        </router-link>
+      </div>
+      <div class="nav_bar_action_frame">
+        <div v-if="logged_in === false">
+          <router-link tag="div" class="nav_bar_action_link" to="/register">
+            注冊
+          </router-link>
+          <router-link tag="div" class="nav_bar_action_link" to="/login">
+            登录
+          </router-link>
+        </div>
+        <div v-else>
+          <router-link
+            tag="div"
+            class="nav_bar_action_link"
+            :to="'/userinfo/' + user_info.id"
+          >
+            个人中心
+          </router-link>
+        </div>
+      </div>
     </div>
     <div class="nav_bar_underline"></div>
   </div>
@@ -27,30 +50,48 @@
 export default {
   name: "NavBar",
   props: [],
-  mounted() {},
+  mounted() {
+    /**
+     * 获取登录状态
+     *
+     */
+    var result = {
+      logged_in: false,
+      user_info: { avatar_url: null, name: null, id: null },
+    };
+    this.logged_in = result.logged_in;
+    this.user_info = result.user_info;
+  },
   data() {
     return {
-        pages:[
-            {
-                title:"首页",
-                url:"/",
-            },
-            {
-                title:"学科",
-                url:"/categories",
-            },{
-                title:"学者",
-                url:"/scholars"    
-            }
-        ],
-        activeIndex:0,
+      pages: [
+        {
+          title: "首页",
+          url: "/",
+        },
+        {
+          title: "学科",
+          url: "/categories",
+        },
+        {
+          title: "学者",
+          url: "/scholars",
+        },
+      ],
+      active_index: 0,
+      logged_in: false,
+      user_info: {
+        avatar_url: null,
+        name: null,
+        id: null,
+      },
     };
   },
   methods: {
-    onIndexChange(i){
-        this.activeIndex = i;
-        this.$root.$router.push(pages[this.activeIndex].url);
-    }
+    onIndexChange(i) {
+      this.active_index = i;
+      this.$root.$router.push(pages[this.active_index].url);
+    },
   },
 };
 </script>
@@ -61,11 +102,11 @@ export default {
   top: 0px;
   height: 46px;
   width: 100%;
-  
-    vertical-align: middle;
+
+  vertical-align: middle;
 }
 .nav_bar_frame {
-    vertical-align: middle;
+  vertical-align: middle;
   height: 45px;
   background-color: white;
   opacity: 50%;
@@ -81,42 +122,50 @@ export default {
   width: 100vw;
 }
 
-.nav_bar_icon{
-    margin-left: 25px;
-    float:left;
-    line-height: 45px;
-    font-family: 'MavenPro';
-    vertical-align: middle;
-    height: 45px;
+.nav_bar_icon {
+  margin-left: 25px;
+  float: left;
+  line-height: 45px;
+  font-family: "MavenPro";
+  vertical-align: middle;
+  height: 45px;
 }
-.nav_bar_icon_title_bold{
-    display: inline;
-    font-weight: bold;
+.nav_bar_icon_title_bold {
+  display: inline;
+  font-weight: bold;
 }
-.nav_bar_icon_title{
-    display: inline;
+.nav_bar_icon_title {
+  display: inline;
 }
-.nav_bar_item_outer_frame{
-        float:left;
-    margin-left: 20px;
+.nav_bar_item_outer_frame {
+  float: left;
+  margin-left: 20px;
 }
-.nav_bar_item_frame{
-    display: inline;
-    color: black;
-    margin:0px 10px 10px 0px;
-    line-height: 45px;
+.nav_bar_item_frame {
+  display: inline;
+  color: black;
+  margin: 0px 10px 10px 0px;
+  line-height: 45px;
 }
-.nav_bar_active_item{
-    display: inline;
-    line-height: 45px;
-    border-bottom-style:outset;
-    border-bottom-color:black;
-    border-bottom-width: 2px;
-    font-weight: bold;
-    height: 45px;
-    width: 100%;
+.nav_bar_active_item {
+  display: inline;
+  line-height: 45px;
+  border-bottom-style: outset;
+  border-bottom-color: black;
+  border-bottom-width: 2px;
+  font-weight: bold;
+  height: 45px;
+  width: 100%;
 }
-.nav_bar_inactive_item{
-    display: inline;
+.nav_bar_inactive_item {
+  display: inline;
+}
+.nav_bar_action_frame {
+  float: right;
+  margin-right: 50px;
+}
+.nav_bar_action_link {
+  display: inline;
+  line-height: 45px;
 }
 </style>
