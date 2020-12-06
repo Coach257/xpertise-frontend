8cb42a5ec24858dd38f49d02c91cf9d742075bb9
Xi Lan <48905285+IAmParasite@users.noreply.github.com>
2020-12-06 14:52:36 +0800

    增加了个人信息修改页面
    增加了个人信息修改页面

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -1,6 +1,18 @@
 <template>
     <div v-title data-title="个人信息页面">
-      <h1>欢迎来到 {} 的主页面</h1>
+      <h1>欢迎来到 {{ this.$route.params.userid }} 的主页面</h1>
+        <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+            <el-form-item label="用户名">
+                <el-input v-model="userinfo.username"></el-input>
+            </el-form-item>
+            <el-form-item label="描述">
+                <el-input v-model="userinfo.description"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱">
+                <el-input v-model="userinfo.email"></el-input>
+            </el-form-item>
+            <el-button type="primary" icon="el-icon-check" @click="saveChange('userinfo')">保存</el-button>
+        </el-form>
     </div>
 </template>
 
@@ -8,4 +20,80 @@
   export default {
     name: 'UserInfo'
   }
+</script>
+
+<script>
+import axios from 'axios';
+export default {
+    name : 'UserInfo',
+    data() {
+        return {
+            labelPosition: 'right',
+            userinfo: {
+                username: '',
+                password: '',
+                email: '',
+                description: '',
+            },
+        };
+        
+    },
+    methods: {
+        saveChange(formName) {
+            var _this = this;
+            this.$refs[formName].validate(valid => {
+            if (valid) {
+            let formData = new FormData();
+            formData.append('new_username', this.userinfo.username);
+            formData.append('new_password1', this.userinfo.password);
+            formData.append('new_email', this.userinfo.email);
+            formData.append('userid', this.localStorage['userid']);
+            formData.append('new_description', this.ruleForm.description);
+            let config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            };
+            axios.post('http://localhost:5000/api/modify_user_info/',formData,config)
+                .then(function (response) {
+                    if (response.data.message=="success"){
+                        localStorage.setItem('token',_this.ruleForm.username);
+                        _this.successmsg("修改成功")
+                        setTimeout(() => {
+                        myrefresh();
+                        }, 2000);
+                    }else {
+                        _this.errormsg("用户名或邮箱已存在")
+                    }
+                })
+                .catch(function () {
+                    _this.errormsg("未知错误，请稍后重试")
+                });
+            } else {
+            return false;
+            }
+        });
+        }
+    },
+    mounted() {
+        this.userinfo.username = this.$route.params.userid;
+        let formData = new FormData();
+        formData.append('userid',this.$route.params.userid);
+        console.log(this.$route.params.userid);
+        let config = {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+        };
+        var _this = this;
+        axios.post('https://go-service-296709.df.r.appspot.com/api/v1/user/getinfo',formData,config)
+        .then(function(response) {
+            if(response) {
+            _this.userObj = response.data;
+            }
+        }).catch(error=> {
+            console.log('error', error);
+        })
+  }
+}
 </script>
