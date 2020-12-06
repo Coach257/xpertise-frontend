452c2ce021c7fc867cf00d3d62474b05646968da
ZhuChenyuCZ <1125664127@qq.com>
2020-12-06 22:18:21 +0800

    添加导航栏组件到app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <NavBar/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import NavBar from './components/common/NavBar.vue'
 export default {
   name: 'App',
+  components:{
+    NavBar,
+  },
   provide () {
     return {
       sleep: this.sleep
