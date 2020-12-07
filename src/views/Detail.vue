9553eaf6ea7bea8de0c7f508793c13f4e44e69e9
ZhuChenyuCZ <1125664127@qq.com>
2020-12-07 22:31:12 +0800

    详情页Detail page

---
+++ src/views/Detail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  <h1>Detail Page</h1>
+  <h1>欢迎来到 {{ this.$route.params.docid }} 的详情页</h1>
+  <h1>title</h1>
+  <h1>摘要</h1>
+  <h1>信息</h1>
+  <h1>版本历史</h1>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Detail',
+   
+  }
+</script>
+
+<style>
+</style>
