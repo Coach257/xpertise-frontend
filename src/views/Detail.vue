4218826f18e0a4eac8e5f5d5ed6e5e7ea3fa061c
ZhuChenyuCZ <1125664127@qq.com>
2020-12-07 21:18:45 +0800

    debug again

---
+++ src/views/Detail.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+  <h1>Detail Page2</h1>
+
+  <RelationMap></RelationMap>
+
+  </div>
+</template>
+
+<script>
+  import RelationMap from '../components/common/RelationMap.vue'
+  export default {
+    name: 'Detail',
+    components: {
+      SelectBarTemplate,
+      RelationMap
+    }
+  }
+</script>
+
+<style>
+</style>
