362182c0b45b1af57d724884bf6888c95602863a
BingOAO233 <wyhchris@qq.com>
2020-12-07 21:30:50 +0800

    admin init

--- src/views/AdminManage.vue
+++ src/views/AdminManage.vue
@@ -1,11 +1,27 @@
 <template>
-    <div v-title data-title="管理页面">
-      <h1>AdminManagePage</h1>
-    </div>
+  <div data-title="管理页面">
+    <!--      <h1>AdminManagePage</h1>-->
+    <el-container>
+      <el-header>
+        <NavBar></NavBar>
+      </el-header>
+      <el-container>
+        <el-aside>
+
+        </el-aside>
+        <el-main>
+
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'AdminManagePage'
-  }
+import NavBar from "@/components/common/NavBar";
+
+export default {
+  name: 'AdminManagePage',
+  components: {NavBar}
+}
 </script>
