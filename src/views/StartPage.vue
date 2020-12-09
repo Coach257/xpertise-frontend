71a3a87a933ae79ebecc605037155a51459c13dd
liukunhao <954001442@qq.com>
2020-12-09 10:50:47 +0800

    startpage

--- src/views/StartPage.vue
+++ src/views/StartPage.vue
@@ -7,11 +7,9 @@
             <span style="font-weight: bold; font-size: 40px; margin-right: 10px;">Xpertise</span>  <span style="font-size: 20px;">Scholar</span>
         </div>
 
-        <input v-model='input' style="border-radius: 10px;">
-
+        <input id="searchinput" v-model='input' v-on:keyup.13="submit">
+            <i class="el-icon-search"></i>
         </input>
-
-        <el-button @click='submit()'>submit</el-button>
 
       </div>
 
@@ -33,18 +31,42 @@
     methods: {
       submit () {
         console.log(this.input)
+        this.$router.push('/')
       }
     }
   }
 </script>
 
 <style scoped>
+#title {
+  margin-top: 120px;
+}
+
+#searchinput {
+  /* outline: #21ff06 dotted thick; */
+
+  border-radius: 30px;
+  border: #cccccc solid thin;
+  width: 500px;
+  height: 36px;
+
+  margin-top: 60px;
+}
+.el-icon-search {
+  font-size: 20px;
+  color: rgb(127,127,127,0.6);
+
+  position: relative;
+  top: -27px;
+  left: 220px;
+}
 
 #backpic {
-  outline: #21ff06 dotted thick;
+  /* outline: #21ff06 dotted thick; */
 
   border: #cccccc solid thin;
   border-radius: 300px;
+  box-shadow: 0px 0px 50px 10px rgb(127,127,127,0.3);
 
   background-image: url(../assets/startBack.png);
   background-size:100% 100%;
@@ -58,6 +80,6 @@
   align-items: center;
 
   position: relative;
-  top: 17%;
+  top: 20%;
 }
 </style>
