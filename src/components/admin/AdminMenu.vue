a93fbb8e41811bfc92d76c934a0a1bac4ba70f18
BingOAO233 <wyhchris@qq.com>
2020-12-10 15:22:19 +0800

    menu subrouter bug fix

--- src/components/admin/AdminMenu.vue
+++ src/components/admin/AdminMenu.vue
@@ -2,13 +2,12 @@
   <div>
     <div class="aside_menu">
       <el-menu active-text-color="#1A1A1A"
-              text-color="#5E5E5E"
-              mode="vertical"
-              :default-active="activeIndex"
-              @select="handleSelect"
-              router
-      >
-        <el-menu-item index="application-manage">
+               text-color="#5E5E5E"
+               mode="vertical"
+               :default-active="this.$route.path"
+               @select="handleSelect"
+               router>
+        <el-menu-item index="/admin/application-manage">
           <i class=""></i>
           <span slot="title">申请处理</span>
         </el-menu-item>
@@ -32,33 +31,36 @@
 </template>
 
 <script>
-  export default {
-    name: "AdminMenu",
-    data() {
-      return {
-        activeIndex: 'report-manage',
-      };      
-    },
-    methods: {
-      handleSelect(key) {
-        console.log(key)
-        switch(key) {
-          case "report-manage":
-            console.log("举报处理");
-            this.$router.push('/report-manage')
-            break;
-          case "application-manage":
-            console.log("申请处理");
-            this.$router.push('/application-manage')
-            break;
-          case "settle-manage":
-            console.log("入驻处理");
-            this.$router.push('/settle-manage')
-            break;
-        }
+export default {
+  name: "AdminMenu",
+  data() {
+    return {
+      activeIndex: 'report-manage',
+    };
+  },
+  mounted() {
+    console.log(this.$route.path)
+  },
+  methods: {
+    handleSelect(key) {
+      console.log(key)
+      switch (key) {
+        case "report-manage":
+          console.log("举报处理");
+          this.$router.push('/report-manage')
+          break;
+        case "application-manage":
+          console.log("申请处理");
+          this.$router.push('/application-manage')
+          break;
+        case "settle-manage":
+          console.log("入驻处理");
+          this.$router.push('/settle-manage')
+          break;
       }
     }
   }
+}
 </script>
 
 
