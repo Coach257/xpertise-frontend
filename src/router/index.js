05da7766ae55d0b9b50481707c00cd7023591b56
BingOAO233 <wyhchris@qq.com>
2020-12-10 15:21:05 +0800

    subrouter bug fix

--- src/router/index.js
+++ src/router/index.js
@@ -48,28 +48,31 @@ const routes = [
     name: 'AdminManage',
     component: AdminManage,
     meta: {title: "管理"},
-    childern: [
+    children: [
       {
-        path: '/report-manage',
+        name: 'ReportManage',
+        path: 'report-manage',
         component: () => import('../components/admin/ReportManage.vue'),
       },
       {
-        path: '/application-manage',
+        name: 'ApplicationManage',
+        path: 'application-manage',
         component: () => import('../components/admin/ApplicationManage.vue'),
       },
       {
-        path: '/settle-manage',
+        name: 'SettleManage',
+        path: 'settle-manage',
         component: () => import('../components/admin/SettleManage.vue'),
       }
     ]
   },
   {
-    path:'/navbar',
-    name:'NavBarTemplate',
+    path: '/navbar',
+    name: 'NavBarTemplate',
     component: NavBarTemplate,
-  },{
-    path:'/article/:articleid',
-    name:'ResultDetailPage',
+  }, {
+    path: '/article/:articleid',
+    name: 'ResultDetailPage',
     component: ResultDetailPage,
   },
   {
@@ -87,7 +90,7 @@ const routes = [
     path: '/userpage/:userid',
     name: 'UserPage',
     component: UserPage,
-    meta: { title: "个人主页"}
+    meta: {title: "个人主页"}
   },
   {
     path: '/portal',
@@ -110,25 +113,25 @@ const router = new VueRouter({
 export default router
 
 router.beforeEach((to,from,next) => {
-//   //to到哪儿  from从哪儿离开  next跳转 为空就是放行  
+//   //to到哪儿  from从哪儿离开  next跳转 为空就是放行
      if (to.path === '/login' || to.path === '/') {
-       //如果跳转为登录，就放行 
+       //如果跳转为登录，就放行
        next();
-    } 
+    }
     else if(to.path === '/register')
       next();
     else if(to.path === '/home')
       next();
     else {
 //     //取出localStorage判断
-          let token = localStorage.getItem('userid');            
-            if (token == null || token === '') { 
-                console.log('请先登录3')    
+          let token = localStorage.getItem('userid');
+            if (token == null || token === '') {
+                console.log('请先登录3')
                 console.log(to.path)
                 alert("请先登录！")
                 //next({name:'loginView'});
-              } 
-              else 
-                next();          
+              }
+              else
+                next();
    }}
 );
