7d50779017491f8906a105e84805643e03d07e43
liukunhao <954001442@qq.com>
2020-12-16 17:20:07 +0800

    little fix

--- src/views/Affiliation.vue
+++ src/views/Affiliation.vue
@@ -2,7 +2,7 @@
   <div id='root'>
 
     <div id='trigger'> </div>
-    <div style="position: fixed;">
+    <div style="position: fixed; top: 70px;">
     <div id='affiliationHeader'>
 
     <svg id='affsvg' class="icon" width="35px" height="35px" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M969.176313 950.856281l3.364561-580.715848a88.100288 88.100288 0 0 0-89.745997-86.19858h-126.756164v-197.485079a84.552871 84.552871 0 0 0-26.258201-61.403232A91.391706 91.391706 0 0 0 666.292716 0.002194H141.274975a91.391706 91.391706 0 0 0-63.268368 25.051348 84.845442 84.845442 0 0 0-24.978206 61.147232V952.319133h-14.482239A37.778164 37.778164 0 0 0 0.036571 988.159018c0.365713 9.508541 4.388557 18.505083 11.263964 25.051348 7.314262 6.98512 17.115374 10.861679 27.245627 10.788537h948.001524C1007.210477 1023.998903 1024.03328 1007.1761 1024.03328 986.403595c0-20.772505-16.822803-37.595308-37.485594-37.595308L969.176313 950.856281zM311.039 421.523125h196.753654c20.699362 0 37.485594 16.822803 37.485593 37.595308 0 20.772505-16.786232 37.595308-37.485593 37.595308H311.258428a38.76559 38.76559 0 0 1-33.75532-20.553077 35.657028 35.657028 0 0 1 0-36.351883c7.204548-11.556534 19.894793-18.505083 33.499321-18.285656z m196.753654 345.086891H311.258428c-20.735933 0-37.522165-16.822803-37.522165-37.595308 0-20.735933 16.822803-37.558736 37.522165-37.558736h196.753654c13.67767-0.329142 26.5142 6.619407 33.718748 18.285655 7.423976 11.812533 7.314262 26.879914-0.219428 38.582733a38.729018 38.729018 0 0 1-33.75532 18.285656z" fill="#666666"/></svg>
@@ -52,7 +52,7 @@
             >
 
 
-              <div id='paperindex'>{{index+(currentPage1-1)*eachPage}}</div>
+              <div id='paperindex'>{{index+1+(currentPage1-1)*eachPage}}</div>
               <div style="width: 700px;"> {{ pub.title }} </div>
 
 
@@ -92,7 +92,7 @@
             tag="a"
             >
 
-            <div id='authorindex'>{{index+(currentPage2-1)*eachPage}}</div>
+            <div id='authorindex'>{{index+1+(currentPage2-1)*eachPage}}</div>
             <div style="width: 200px;"> {{ author.name }} </div>
 
           </router-link>
@@ -128,7 +128,6 @@ import {
   csaffiliationconfig,
 } from "../searchConfig";
 var driver = null;
-
 export default {
   name: "Affiliation",
   data() {
@@ -140,10 +139,8 @@ export default {
         n_pubs: 1,
         name: "NUS",
       },
-
       contendLoaded: false,
       searchState: {},
-
       currentPage1: 1,
       currentPage2: 1,
       eachPage: 50,
@@ -158,7 +155,6 @@ export default {
     driver.subscribeToStateChanges((state) => {
       this.searchState = state;
     })
-
     this.initAnimation()
   },
   methods: {
@@ -191,7 +187,6 @@ export default {
     handleCurrentChange2 (currpage) {
       this.currentPage2 = currpage
     }
-
   },
   watch: {
     searchState(newsearchState) {
@@ -210,7 +205,6 @@ export default {
           this.affiliation.n_citation = results.n_citation.raw;
         if(results.n_pubs)
           this.affiliation.n_pubs = results.n_pubs.raw;
-
         if(results.pubs) {
           raw = results.pubs.raw;
           for(let i = 0; i < raw.length; i++)
@@ -223,13 +217,7 @@ export default {
     }
   },
   computed: {
-
   },
-
-
-
-
-
 }
 </script>
 
@@ -244,14 +232,10 @@ export default {
   border: #e6e6e6 solid thin;
   border-radius: 30px;
   box-shadow: 0px 0px 50px 10px rgba(127,127,127,0.2);
-
   backdrop-filter: blur(20px);
   background-color: rgba(255,255,255,0.5);
-
-
   width: 500px;
   height: 200px;
-
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -267,18 +251,14 @@ export default {
   font-size: 30px;
   font-weight: bolder;
   position: relative;
-
   width: 360px;
-
   text-align:center;
 }
 #affiliationInfo {
   display: flex;
   justify-content: space-between;
-
   color: #666666;
   font-weight: 500;
-
   width: 160px;
   margin-top: 20px;
 }
@@ -286,45 +266,36 @@ export default {
   /* outline: #21ff06 dotted thick; */
   display: flex;
   justify-content: center;
-
   width: 1200px;
 }
 .datawrapper {
   border: #e6e6e6 solid thin;
   border-radius: 30px;
   box-shadow: 0px 0px 50px 10px rgba(127,127,127,0.2);
-
   padding: 20px;
   margin: 40px;
 }
 .datatitle {
   /* outline: #21ff06 dotted thick; */
-
   height: 40px;
   margin-top: 10px;
   padding: 5px;
-
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
 #paperindex{
   /* outline: #21ff06 dotted thick; */
-
   border-radius: 50px;
   background-color: #26BEB8;
   color: white;
-
   width: 30px;
   height: 30px;
-
   font-size: 15px;
   font-weight: bold;
-
   display: flex;
   align-items: center;
   justify-content: center;
-
   margin: 4px;
   margin-right: 10px;
 }
@@ -332,17 +303,13 @@ export default {
   border-radius: 50px;
   background-color: #dace0a;
   color: white;
-
   width: 32px;
   height: 32px;
-
   font-size: 15px;
   font-weight: bold;
-
   display: flex;
   align-items: center;
   justify-content: center;
-
   margin: 4px;
   margin-right: 10px;
 }
