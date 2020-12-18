3b823486891b9b8ddeb8a90aa137ba60f3ffe388
BingOAO233 <wyhchris@qq.com>
2020-12-18 18:07:12 +0800

    chart component init

---
+++ src/components/common/PaperChart.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="paper-chart"></div>
+</template>
+
+<script>
+import $ from 'jquery'
+
+export default {
+  name: "PaperChart",
+  data() {
+    return {
+      option: {}
+    }
+  },
+  mounted() {
+    let echarts = require('echarts')
+
+    let myChart = echarts.init($("#paper-chart"))
+
+    myChart.setOption(this.option)
+  }
+}
+</script>
+
+<style scoped>
+#paper-chart {
+  width: 600px;
+  height: 400px;
+}
+</style>
