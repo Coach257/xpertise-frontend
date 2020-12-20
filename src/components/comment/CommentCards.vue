590f1b7951cbdfa16e4a782f9d59140857ca150d
Xi Lan <48905285+IAmParasite@users.noreply.github.com>
2020-12-20 22:28:14 +0800

    增加了评论点赞/踩，评论显示功能：

--- src/components/comment/CommentCards.vue
+++ src/components/comment/CommentCards.vue
@@ -1,22 +1,56 @@
 <template>
-  <div>
-    我是小熊的小迷弟
+  <div style="width:100%;">
+    <CommentCard v-for="(comment,index) in commentList" :key="index" :comment="comment"
+                  :index="index"></CommentCard>
   </div>
 </template>
 
 <script>
+import CommentCard from '@/components/comment/CommentCard'
+import axios from 'axios'
+
+const testurl = "https://go-service-296709.df.r.appspot.com/api/v1/branch/comment/list_all_comments"
 export default {
   name: "CommentCards",
   props: {
     id: String
   },
+  components: {
+    CommentCard,
+  },
   data() {
     return{
-
+      commentList: [],
     }
   },
   mounted () {
     console.log(this.id);
+    let that = this;
+    let formData = new FormData();
+    formData.append("paper_id", this.id);
+    let config = { headers: { "Content-Type": "multipart/form-data", }, };
+    axios.post(testurl, formData, config).then((response) => {
+        if (response) {
+          if (response.data.success) {
+            let list = response.data.data;
+            for (let i = 0; i < list.length; i++) {
+              this.commentList.push({
+                create_time: list[i]['comment_time'],
+                content: list[i]['content'],
+                dislike: list[i]['dislike'],
+                like: list[i]['like'],
+                username: list[i]['username'],
+                comment_id: list[i]['comment_id'],
+                on_top: list[i]['on_top'],
+                paper_id: list[i]['paper_id'],
+                user_id: list[i]['user_id'],
+              })
+            }
+          } else {
+            console.log(response)
+          }
+        }
+      });
   },
   methods: {
 
