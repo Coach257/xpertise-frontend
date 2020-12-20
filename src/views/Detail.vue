fbcad5b4385bfd699153b0d675b9c2ee48f87f6f
Coach257 <45021066+Coach257@users.noreply.github.com>
2020-12-20 12:58:47 +0800

    add: detail 逻辑布局 完成相关文章

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="result_detail_page" v-if="loadfinish">
+  <div id="result_detail_page" v-if="articleloadfinish">
     <div class="result_detail_page_container">
       <div class="result_detail_title_area">
         <div class="result_detail_title">
@@ -66,9 +66,11 @@
             @click="removeFromFav"
             >已收藏</el-button
           >
-          <el-button type="warning" @click="showInfo">显示信息</el-button>
           <h3>相关文章</h3>
-
+          <li v-for="result in this.related_papers.slice(1)" :key="result.id.raw">
+            <!-- <SearchResult :type="this.type" :option="this.option" :result="result" /> -->
+            {{result.title.raw}}
+          </li>
           <div v-if="referenceloadfinish">
             <h3>引用关系图谱</h3>
             这里要加引用关系图谱
@@ -84,6 +86,7 @@
 </template>
 <script>
 import { SearchDriver } from "@elastic/search-ui";
+import SearchResults from "../components/search/SearchResults";
 import {
   mainpaperconfig,
   mainauthorconfig,
@@ -97,20 +100,14 @@ var driver = null;
 export default {
   name: "Detail",
   props: [],
-  components: {},
+  components: {
+    SearchResults
+  },
   mounted() {
-    console.log(this.$route.params.type);
-    if (this.$route.params.type == "cs") {
-      driver = new SearchDriver(cspaperconfig);
-    } else {
-      driver = new SearchDriver(mainpaperconfig);
-    }
-    driver.reset();
-    driver.setResultsPerPage(1);
-    driver.addFilter("id", this.$route.params.docid, "any");
-    driver.subscribeToStateChanges((state) => {
-      this.searchState = state;
-    });
+    this.init_data();
+    this.init_driver();
+    this.loadcomment();
+    this.loadrecommand();
   },
   data() {
     return {
@@ -135,80 +132,67 @@ export default {
         listed: false,
         starred: false,
       },
+      docid: "",
+      type: "",
+      option: "",
+      related_papers: [],
       searchState: {},
-      referenceloaded: false,
-      articleloaded: false,
+      driverlink: "", // 控制es结果赋值
+      referenceloaded: false, // 控制引用图谱显示
+      articleloaded: false, // 控制整个页面显示
+      relatedloaded: false, // 控制相关文章显示
     };
   },
   watch: {
     searchState(newsearchState) {
       if (this.thereAreResults()) {
-        // 任意一种result都有可能没有任意一种属性,任意一种属性的值都有可能为空
-        var results = newsearchState.results[0];
-        var raw;
-        this.article.paper_id = results.id.raw;
-        this.isFav();
-        console.log(this.article.starred);
-        // 更新标题
-        if (results.title) this.article.title = results.title.raw;
-        // 更新作者
-        if (results.authors) {
-          raw = results.authors.raw;
-          for (var i = 0; i < raw.length; i++) {
-            this.article.authors.push(JSON.parse(raw[i]));
-          }
+        if (this.driverlink == "thispaper") {
+          this.getthispaper();
+        } else {
+          this.getrelatedpaper();
         }
-        // 更新摘要
-        if (results.abstract) this.article.abstract = results.abstract.raw;
-        // 更新发布年份
-        if (results.year) this.article.year = results.year.raw;
-        // 更新关键词
-        if (results.keywords) {
-          raw = results.keywords.raw;
-          for (var i = 0; i < raw.length; i++) {
-            this.article.keywords.push(raw[i]);
-          }
-        }
-        // 更新引用数
-        if (results.n_citation)
-          this.article.n_citation = results.n_citation.raw;
-        // 更新页
-        if (results.page_start) {
-          this.article.page_start = results.page_start.raw;
-          this.article.page_end = results.page_end.raw;
-        }
-        if (results.lang) this.article.lang = results.lang.raw;
-        if (results.issue) this.article.issue = results.issue.raw;
-        if (results.venue) this.article.venue = results.venue.raw;
-        if (results.conference)
-          this.article.conference = results.conference.raw;
-        if (results.volume) this.article.volume = results.volume.raw;
-        if (results.issn) this.article.issn = results.issn.raw;
-        if (results.doi) this.article.doi = results.doi.raw;
-        if (results.url) this.article.url = results.url.raw;
-        if (this.$route.params.type == "cs") {
-          this.loadreference();
-        }
-        this.articleloaded = true;
       }
-      driver = null;
     },
   },
   computed: {
     //computed最高优先级，只有当loadfinish为true时,才开始页面加载
-    loadfinish() {
+    articleloadfinish() {
       return this.articleloaded;
     },
     referenceloadfinish() {
       return this.referenceloaded;
     },
+    relatedpaperloadfinish() {
+      return this.relatedloaded;
+    }
   },
   methods: {
+    // 初始化全局数据
+    init_data() {
+      this.type = this.$route.params.type;
+      this.option = "paper";
+      this.docid = this.$route.params.docid;
+    },
+    // 配置es连接器
+    init_driver() {
+      this.driverlink = "thispaper";
+      if (this.type == "cs") {
+        driver = new SearchDriver(cspaperconfig);
+      } else {
+        driver = new SearchDriver(mainpaperconfig);
+      }
+      driver.reset();
+      driver.setResultsPerPage(1);
+      driver.addFilter("id", this.docid, "any");
+      driver.subscribeToStateChanges((state) => {
+        this.searchState = state;
+      });
+    },
+    // 添加收藏
     addToFav() {
       let that = this;
       let formData = new FormData();
       formData.append("user_id", localStorage.getItem("userid"));
-      console.log(this.$data);
       formData.append("paper_id", this.article.paper_id);
       let paperInfo = {
         paper_id: this.article.paper_id,
@@ -216,7 +200,6 @@ export default {
         authors: JSON.parse(JSON.stringify(this.article.authors)),
       };
       formData.append("paper_info", JSON.stringify(paperInfo));
-      console.log(paperInfo);
       let config = { headers: { "Content-Type": "multipart/form-data" } };
       axios
         .post(
@@ -226,9 +209,7 @@ export default {
         )
         .then((response) => {
           if (response) {
-            console.log("收到请求", response);
             if (response.data.success) {
-              console.log("收藏成功", response.data);
               that.$data.article.starred = true;
             } else {
               console.log("收藏失败" + response.data);
@@ -240,6 +221,7 @@ export default {
           }
         });
     },
+    // 取消收藏
     removeFromFav() {
       let that = this;
       let formData = new FormData();
@@ -287,30 +269,72 @@ export default {
                 }
               }
             } else {
-              console.log(response.data);
               console.log("获取失败 " + response.data);
               alert("收藏列表获取失败，请检查网络");
             }
           } else {
-            console.log(response.data);
             console.log("获取失败 " + response.data);
             alert("收藏列表获取失败，请检查网络");
           }
         });
     },
+    // 搜索引擎是否返回结果
     thereAreResults() {
       return this.searchState.totalResults && this.searchState.totalResults > 0;
+    },
+    // 赋值article
+    getthispaper(){
+      if(this.type=="cs") this.loadreference();
+      var results = this.searchState.results[0];
+      var raw;
+      this.article.paper_id = results.id.raw;
+      this.isFav();
+      if (results.title && results.title.raw) this.article.title = results.title.raw;
+      if (results.authors && results.authors.raw) {
+        raw = results.authors.raw;
+        for (var i = 0; i < raw.length; i++) {
+          this.article.authors.push(JSON.parse(raw[i]));
+        }
+      }
+      if (results.abstract && results.abstract.raw) this.article.abstract = results.abstract.raw;
+      if (results.year && results.year.raw) this.article.year = results.year.raw;
+      if (results.keywords && results.keywords.raw) {
+        raw = results.keywords.raw;
+        for (var i = 0; i < raw.length; i++) {
+          this.article.keywords.push(raw[i]);
+        }
+      }
+      if (results.n_citation && results.n_citation.raw) this.article.n_citation = results.n_citation.raw;
+      if (results.page_start && results.page_start.raw) this.article.page_start = results.page_start.raw;
+      if (results.page_end && results.page_end.raw) this.article.page_end = results.page_end.raw;
+      if (results.lang && results.lang.raw) this.article.lang = results.lang.raw;
+      if (results.issue && results.issue.raw) this.article.issue = results.issue.raw;
+      if (results.venue && results.venue.raw) this.article.venue = results.venue.raw;
+      if (results.conference && results.conference.raw) this.article.conference = results.conference.raw;
+      if (results.volume && results.volume.raw) this.article.volume = results.volume.raw;
+      if (results.issn && results.issn.raw) this.article.issn = results.issn.raw;
+      if (results.doi && results.doi.raw) this.article.doi = results.doi.raw;
+      if (results.url && results.url.raw) this.article.url = results.url.raw;
+      this.articleloaded = true;
+      this.loadrelatedpapers();
+    },
+    // 获取相关文章
+    loadrelatedpapers(){
+      this.driverlink = "related"
+      driver.reset();
+      driver.setResultsPerPage(6);
+      driver.getActions().setSearchTerm(this.article.title);
+    },
+    // 赋值相关文章
+    getrelatedpaper(){
+      this.related_papers = this.searchState.results;
+      this.relatedloaded = true;
     },
     loadreference() {
       console.log("这个函数请求引用关系");
       this.referenceloaded = true;
     },
-    showInfo() {
-      console.log(this.searchState);
-      console.log(this.article.authors);
-      console.log(this.authors);
-      console.log(JSON.parse(this.searchState.results[0].authors.raw[0]));
-    },
+    // 判断该文章是否被收藏
     isFav() {
       let that = this;
       let formData = new FormData();
@@ -339,6 +363,10 @@ export default {
         });
       return;
     },
+    // 加载评论
+    loadcomment() {},
+    // 加载推荐
+    loadrecommand() {},
   },
 };
 </script>
