7ec747052567edb63a41e00beb6a2d7c35cd6f9d
BingOAO233 <wyhchris@qq.com>
2020-12-21 12:39:19 +0800

    :wyh: axios code style fix

--- src/components/admin/RequestManage.vue
+++ src/components/admin/RequestManage.vue
@@ -25,60 +25,40 @@ export default {
   },
   mounted() {
     // TODO: request info parse
-    var _this = this;
-    axios.get(testUrl).then(response => {
-      if (response) {
-        if (response.data.success == true) {
-          console.log(response.data.data);
-          let list = response.data.data;
-          for (let i = 0; i < list.length; i++) {
-            this.requestList.push({
-              requestId: list[i]['authreq_id'],
-              userId: list[i]['user_id'],
-              citizen_id: list[i]['citizen_id'],
-              requestTime: list[i]['request_time'],
-              organization: list[i]['organization']
-            })
-          }
-        } else {
-          for (let i = 0; i < 4; i++) {
-            this.requestList.push({
-              requestId: -1,
-              userId: 0,
-              citizen_id: "110000100001010000",
-              requestTime: "XXXX-XX-XX xx:xx:xx",
-              organization: "DEBUG XXX XX CN XXXX"
-            })
-          }
-          console.log(response.data)
+    this.$axios.get(testUrl).then((res) => {
+      if (res.data.success) {
+        let list = res.data.data
+        for (let i = 0; i < list.length; i++) {
+          this.requestList.push({
+            requestId: list[i]['authreq_id'],
+            userId: list[i]['user_id'],
+            citizen_id: list[i]['citizen_id'],
+            requestTime: list[i]['time'],
+            organization: list[i]['org']
+          })
         }
       } else {
-        console.log("error");
+        this.$notify({
+          title: "错误",
+          message: h("div", {class: 'el-icon-close', style: 'color: red'}, " 系统好像出了点问题?"),
+        })
       }
+    }).catch(err => {
+      console.log(err)
+      for (let i = 0; i < 4; i++) {
+        this.requestList.push({
+          requestId: -1,
+          userId: 0,
+          citizen_id: "110000100001010000",
+          requestTime: "XXXX-XX-XX xx:xx:xx",
+          organization: "DEBUG XXX XX CN XXXX"
+        })
+      }
+      this.$notify({
+        title: "网络错误",
+        message: h("div", {class: 'el-icon-close', style: 'color: red'}, " 网络好像出了点问题，错误码:" + err.status),
+      })
     })
-    // this.$axios.get(testUrl).then((res) => {
-    //   let list = res['data']
-    //   for (let i = 0; i < list.length; i++) {
-    //     this.requestList.push({
-    //       requestId: list[i]['authreq_id'],
-    //       userId: list[i]['user_id'],
-    //       citizen_id: list[i]['citizen_id'],
-    //       requestTime: list[i]['time'],
-    //       organization: list[i]['org']
-    //     })
-    //   }
-    // }).catch(err => {
-    //   console.log(err)
-    //   for (let i = 0; i < 4; i++) {
-    //     this.requestList.push({
-    //       requestId: -1,
-    //       userId: 0,
-    //       citizen_id: "110000100001010000",
-    //       requestTime: "XXXX-XX-XX xx:xx:xx",
-    //       organization: "DEBUG XXX XX CN XXXX"
-    //     })
-    //   }
-    // })
     this.$nextTick(() => {
       this.isScreenWide = window.innerWidth > 1300
       $(".request-card").css("width", this.isScreenWide ? "30%" : "45%")
