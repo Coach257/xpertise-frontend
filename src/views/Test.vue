c785aa64c6207084b1a5975aa6af41e9d3cbbd79
BingOAO233 <wyhchris@qq.com>
2020-12-21 22:33:02 +0800

    :wyh: add AuthorCompareChart Example

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -6,6 +6,7 @@
     <organization-year-paper-chart :type="'Citation'"></organization-year-paper-chart>
     <organization-year-paper-chart :type="'Paper'"></organization-year-paper-chart>
     <author-year-paper-chart></author-year-paper-chart>
+    <author-compare-chart></author-compare-chart>
 
 
     <SelectTitle :buttons='buttons'/>
@@ -18,10 +19,12 @@ import RelationMap from '../components/common/RelationMap.vue'
 import OrganizationYearPaperChart from "@/components/common/OrganizationYearPaperChart";
 import AuthorYearPaperChart from "@/components/common/AuthorYearPaperChart";
 import SelectTitle from '../components/common/SelectTitle.vue'
+import AuthorCompareChart from "@/components/common/AuthorCompareChart";
 
 export default {
   name: 'Test',
   components: {
+    AuthorCompareChart,
     AuthorYearPaperChart,
     OrganizationYearPaperChart,
     RelationMap,
