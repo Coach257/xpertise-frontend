ab9f4f868b01cb67abff6bad7f857985f06e7149
BingOAO233 <wyhchris@qq.com>
2020-12-23 18:20:57 +0800

    :wyh: add component: LoadingChart.vue(always show loading animation)

---
+++ src/components/common/LoadingChart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    :id="'loading-chart-container'+this.id"
+    style="width: 600px; height: 500px"
+  ></div>
+</template>
+
+
+<script>
+export default {
+  name: "LoadingChart",
+  props: {
+    id: Number
+  },
+  mounted() {
+    let mychart = this.$echarts.init(document.getElementById('loading-chart-container' + this.id))
+    mychart.showLoading()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
